6569eb982668b0839962389a2313b1432d3dee8e
neilgupte <neil.gupte@gmail.com>
2025-04-23 17:14:02 +0100

    Update LabourPotential.tsx

--- src/pages/LabourPotential.tsx
+++ src/pages/LabourPotential.tsx
@@ -1,13 +1,9 @@
-import LabourSidebar from "@/components/labour-potential/LabourSidebar";
 import { Outlet } from "react-router-dom";
 
 const LabourPotential = () => {
   return (
-    <div className="flex h-full min-h-[calc(100vh-4rem)]">
-      <LabourSidebar />
-      <div className="flex-1 overflow-auto">
-        <Outlet />
-      </div>
+    <div className="flex-1 overflow-auto">
+      <Outlet />
     </div>
   );
 };
