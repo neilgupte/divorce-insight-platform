8fd9c0c403f209142c1dd70a9c6194962669f72d
neilgupte <neil.gupte@gmail.com>
2025-04-26 11:39:52 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Or your root component
+import App from './App';
 import './index.css';
 
 const queryClient = new QueryClient();
