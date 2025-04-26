f43af49f555b7801da692db0a6f6bf82dc0cfc25
neilgupte <neil.gupte@gmail.com>
2025-04-26 11:30:54 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'; // Or your root component
+import './index.css';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// 1. Create the client
 const queryClient = new QueryClient();
 
-// 2. Provide it at the root
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
